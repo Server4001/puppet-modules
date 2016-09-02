eb5023caa1a48ff7a65bb4c6ce07c9e23f5a26c9
Brice Bentler <server4001@gmail.com>
2016-09-01 18:51:37 -0700

    Adds ntp config file for Windows in ntp module

--- puppetmaster-server/puppetlabs/code/environments/production/modules/ntp/manifests/config.pp
+++ puppetmaster-server/puppetlabs/code/environments/production/modules/ntp/manifests/config.pp
@@ -6,11 +6,20 @@
 class ntp::config inherits ntp {
   $servers = $ntp::servers # TODO : Might be able to remove this line w/o issues.
 
-  file { '/etc/ntp.conf':
-    ensure  => file,
-    mode    => '0644',
-    owner   => 'root',
-    group   => 'root',
-    content => template('ntp/ntp.conf.erb'),
+  if ($::kernel == 'Linux') {
+    file { '/etc/ntp.conf':
+      ensure  => file,
+      mode    => '0644',
+      owner   => 'root',
+      group   => 'root',
+      content => template('ntp/ntp.conf.erb'),
+    }
+  }
+  if ($::kernel == 'Windows') {
+    file { 'c:\\ntp\\etc\\ntp.conf':
+      ensure  => file,
+      content => template('ntp/ntp.conf.erb'),
+      require => Package['meinberg-ntp'],
+    }
   }
 }
--- puppetmaster-server/puppetlabs/code/environments/production/modules/ntp/spec/classes/config_spec.rb
+++ puppetmaster-server/puppetlabs/code/environments/production/modules/ntp/spec/classes/config_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 describe 'ntp::config' do
 
-  context 'for a Linux distribution ' do
+  context 'for a Linux distribution' do
     let(:facts) {{ :kernel => 'Linux' }}
 
     it do
@@ -10,6 +10,16 @@ describe 'ntp::config' do
         'mode'    => '0644',
         'owner'   => 'root',
         'group'   => 'root',
+        'content' => /server 0.us.pool.ntp.org iburst\nserver 1.us.pool.ntp.org iburst\nserver 2.us.pool.ntp.org iburst\nserver 3.us.pool.ntp.org iburst/,
+      })
+    end
+  end
+
+  context 'for a Windows distribution' do
+    let(:facts) {{ :kernel => 'Windows' }}
+    it do
+      should contain_file('c:\\ntp\\etc\\ntp.conf').with({
+        'ensure'  => 'file',
         'content' => /server 0.us.pool.ntp.org iburst\nserver 1.us.pool.ntp.org iburst\nserver 2.us.pool.ntp.org iburst\nserver 3.us.pool.ntp.org iburst/,
       })
     end
