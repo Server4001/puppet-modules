63dbc5750f9a3ce08c1156eeacbcd5c532bf6de6
Brice Bentler <server4001@gmail.com>
2016-09-02 23:05:35 -0700

    Adds example file for smoke tests

--- puppetmaster-server/puppetlabs/code/environments/production/modules/ntp/examples/init.pp
+++ puppetmaster-server/puppetlabs/code/environments/production/modules/ntp/examples/init.pp
@@ -9,4 +9,11 @@
 # Learn more about module testing here:
 # https://docs.puppet.com/guides/tests_smoke.html
 #
-include ::ntp
+class { '::ntp':
+  servers => [
+    '0.us.pool.ntp.org',
+    '1.us.pool.ntp.org',
+    '2.us.pool.ntp.org',
+    '3.us.pool.ntp.org',
+  ],
+}
